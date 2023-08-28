155c52a019973308c2764f64c5c5fbd58e595271
ChrisSamHarris <christopherharris1995@ymail.com>
2023-08-28 18:40:07 +0100

    text_conversion completed, moved away from Pandas to extraploate txt data and instead used open(file, 'r') for my own sanity

--- text_conversion/main.py
+++ text_conversion/main.py
@@ -1,23 +1,21 @@
 from fpdf import FPDF
-import pandas as pd
 import glob 
 from pathlib import Path
 
 filepaths = glob.glob("text_conversion/text_files/*txt")
-
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 
 for filepath in filepaths:
-    df = pd.read_csv(filepath)
-    print(df)
+
+    with open(filepath, 'r') as file:
+        content = file.read()
+
     pdf.add_page()
     filename = Path(filepath).stem
     pdf.set_font(family="Times", style="B", size=16)
     pdf.cell(w=50, h=8, txt=filename.title(), align="L", ln=1, border=0)
 
     pdf.set_font(family="Times", style="", size=10)
-    f = open(filepath , "r")
-    for x in f:
-        pdf.cell(200, 10, txt = x, ln = 5, align = 'L')
+    pdf.multi_cell(h=6, w=190, txt=content)
 
 pdf.output("text_conversion/converted_txt.pdf")
