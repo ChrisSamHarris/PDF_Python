75ffb84c50cfbcd486e5a09d2a237c6e7a89805d
ChrisSamHarris <christopherharris1995@ymail.com>
2023-08-28 17:43:00 +0100

    Invoice date added, generation date added - formatting for the PDF

--- excel_conversion/excel_conversion.py
+++ excel_conversion/excel_conversion.py
@@ -2,39 +2,30 @@ from fpdf import FPDF
 import pandas as pd
 import glob 
 from pathlib import Path
+from datetime import date
 
+today = date.today()
+t_date = today.strftime("%d-%m-%Y")
 filepaths = glob.glob("excel_conversion/excel_invoices/*xlsx")
 print(filepaths)
 
-# df_dict = []
-
 for filepath in filepaths:
     df = pd.read_excel(filepath, sheet_name="Sheet 1")
-    # df_dict.append(df)
+
     pdf = FPDF(orientation="P", unit="mm", format="A4")
     pdf.add_page()
+
     filename = Path(filepath).stem
-    # invoice_number = filename.split("-")[0]
-    invoice_number = filename[:5]
-    pdf.set_font(family="Times", style="B", size=16)
+    invoice_number, invoice_date = filename.split("-")
+    invoice_date = invoice_date.replace(".", "-")
+    # invoice_number = filename[:5]
+    
+    pdf.set_font(family="Times", style="B", size=14)
     pdf.cell(w=50, h=8, txt=f"Invoice Number: {invoice_number}", align="L", ln=1, border=0)
+
+    pdf.set_font(family="Times", style="", size=10)
+    pdf.cell(w=50, h=5, txt=f"Invoice Date: {invoice_date}", align="L", ln=1, border=0)
+    pdf.cell(w=50, h=5, txt=f"Generation Date: {t_date}", align="L", ln=1, border=0)
+
     pdf.output(f"excel_conversion/pdf_invoice/test_{invoice_number}.pdf")
-
-
-# print(df_dict)
-
-# num = 0
-# for i in df_dict:
     
-#     pdf.set_auto_page_break(auto=False, margin=0)
-#     number_of_pages = 1
-    
-
-#     # Header
-    
-#     pdf.set_text_color(0,0,0)
-    
-#     pdf.line(x1=10, x2=200, y1=18, y2=18)
-
-#     num += 1
-#     
